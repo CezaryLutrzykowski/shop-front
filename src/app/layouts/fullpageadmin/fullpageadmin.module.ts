c4679eee4f18a41dbdece70661d60fcafa0c199e
Czaro <czareklutrzykowski@gmail.com>
2023-01-13 23:18:39 +0100

    edycja produktu

--- src/app/layouts/fullpageadmin/fullpageadmin.module.ts
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -6,19 +6,23 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import {MaterialModule} from "../../shared/material.module";
 import {AdminProductComponent} from "../../modules/admin/admin-product/admin-product.component";
+import {AdminProductUpdateComponent} from "../../modules/admin/admin-product-update/admin-product-update.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     FullpageadminComponent,
     AdminComponent,
-    AdminProductComponent
+    AdminProductComponent,
+    AdminProductUpdateComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
     FlexLayoutModule,
     MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class FullpageadminModule { }
