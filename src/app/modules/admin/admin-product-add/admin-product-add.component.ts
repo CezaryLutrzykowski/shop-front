94e6dadbc8fcdb0015ced7f6c46a35fd2a878b0e
Czaro <czareklutrzykowski@gmail.com>
2023-02-04 11:15:09 +0100

    upload obrazka przy dodawaniu produktu

--- src/app/modules/admin/admin-product-add/admin-product-add.component.ts
+++ src/app/modules/admin/admin-product-add/admin-product-add.component.ts
@@ -5,6 +5,7 @@ import {AdminProductAddService} from "./admin-product-add.service";
 import {Router} from "@angular/router";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {AdminMessageService} from "../admin-message.service";
+import {AdminProductUpdateService} from "../admin-product-update/admin-product-update.service";
 
 @Component({
   selector: 'app-admin-product-add',
@@ -13,14 +14,19 @@ import {AdminMessageService} from "../admin-message.service";
 })
 export class AdminProductAddComponent implements OnInit {
 
-  product !: AdminProductUpdate;
   productForm!: FormGroup;
+  requiredFileTypes = "image/jpeg, image/png";
+  image: string | null = null;
+  imageForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
-              private adminProductAddService: AdminProductAddService,
-              private router: Router,
-              private snackBar: MatSnackBar,
-              private adminMessageService: AdminMessageService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private adminProductAddService: AdminProductAddService,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private adminMessageService: AdminMessageService,
+    private adminProductUpdateService: AdminProductUpdateService
+  ) {
   }
 
   ngOnInit(): void {
@@ -32,18 +38,47 @@ export class AdminProductAddComponent implements OnInit {
       currency: ['PLN', Validators.required],
       slug: ['', [Validators.required, Validators.minLength(4)]]
     });
+    this.imageForm = this.formBuilder.group({
+      file: ['']
+    })
   }
 
 
   submit() {
-    this.adminProductAddService.saveNewProduct(this.productForm.value)
+    this.adminProductAddService.saveNewProduct({
+      name: this.productForm.get('name')?.value,
+      description: this.productForm.get('description')?.value,
+      fullDescription: this.productForm.get('fullDescription')?.value,
+      category: this.productForm.get('category')?.value,
+      price: this.productForm.get('price')?.value,
+      currency: this.productForm.get('currency')?.value,
+      slug: this.productForm.get('slug')?.value,
+      image: this.image
+    } as AdminProductUpdate)
       .subscribe({
         next: product => {
           this.router.navigate(["/admin/products/update", product.id])
-            .then(() => this.snackBar.open("Product został dodany", "", {duration: 3000}))
+            .then(() => this.snackBar.open("Produkt został dodany", "", { duration: 3000 }))
         },
         error: err => this.adminMessageService.addSpringErrors(err.error)
       })
   }
+
+
+  uploadFile() {
+    let formData = new FormData();
+    formData.append('file', this.imageForm.get('file')?.value);
+    this.adminProductUpdateService.uploadImage(formData)
+      .subscribe(result => this.image = result.filename);
+  }
+
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.imageForm.patchValue({
+        file: event.target.files[0]
+      });
+    }
+  }
+
 
 }
--- src/app/modules/admin/admin-product-form/admin-product-form.component.ts
+++ src/app/modules/admin/admin-product-form/admin-product-form.component.ts
@@ -8,54 +8,60 @@ import {FormGroup} from "@angular/forms";
           <mat-form-field appearance="fill">
               <mat-label>Nazwa</mat-label>
               <input matInput placeholder="Podaj nazwę produktu" formControlName="name">
-            <div *ngIf="name?.invalid && (name?.dirty || name?.touched)" class="errorMessages">
-              <div *ngIf="name?.errors?.['required']">Nazwa jest wymagana</div>
-              <div *ngIf="name?.errors?.['minlength']">Nazwa musi mieć przynajmniej 4 znaki</div>
-            </div>
+              <div *ngIf="name?.invalid && (name?.dirty || name?.touched)" class="errorMessages">
+                  <div *ngIf="name?.errors?.['required']">Nazwa jest wymagana</div>
+                  <div *ngIf="name?.errors?.['minlength']">Nazwa musi mieć przynajmniej 4 znaki</div>
+              </div>
           </mat-form-field>
 
-        <mat-form-field appearance="fill">
-          <mat-label>Przyjazny url</mat-label>
-          <input matInput placeholder="Podaj url" formControlName="slug">
-          <div *ngIf="slug?.invalid && (slug?.dirty || slug?.touched)" class="errorMessages">
-            <div *ngIf="slug?.errors?.['required']">Nazwa jest wymagana</div>
-            <div *ngIf="slug?.errors?.['minlength']">Nazwa musi mieć przynajmniej 4 znaki</div>
-          </div>
-        </mat-form-field>
+          <mat-form-field appearance="fill">
+              <mat-label>Przyjazny url</mat-label>
+              <input matInput placeholder="Podaj url" formControlName="slug">
+              <div *ngIf="slug?.invalid && (slug?.dirty || slug?.touched)" class="errorMessages">
+                  <div *ngIf="slug?.errors?.['required']">Nazwa jest wymagana</div>
+                  <div *ngIf="slug?.errors?.['minlength']">Nazwa musi mieć przynajmniej 4 znaki</div>
+              </div>
+          </mat-form-field>
 
           <mat-form-field appearance="fill">
               <mat-label>Opis</mat-label>
               <textarea matInput rows="20" placeholder="Podaj opis produktu" formControlName="description"></textarea>
-            <div *ngIf="description?.invalid && (description?.dirty || description?.touched)" class="errorMessages">
-              <div *ngIf="description?.errors?.['required']">Opis jest wymagana</div>
-              <div *ngIf="description?.errors?.['minlength']">Opis musi mieć przynajmniej 4 znaki</div>
-            </div>
+              <div *ngIf="description?.invalid && (description?.dirty || description?.touched)" class="errorMessages">
+                  <div *ngIf="description?.errors?.['required']">Opis jest wymagana</div>
+                  <div *ngIf="description?.errors?.['minlength']">Opis musi mieć przynajmniej 4 znaki</div>
+              </div>
+          </mat-form-field>
+
+          <mat-form-field appearance="fill">
+              <mat-label>Pełny opis</mat-label>
+              <textarea matInput rows="40" placeholder="Podaj pełny opis produktu"
+                        formControlName="fullDescription"></textarea>
           </mat-form-field>
 
           <mat-form-field appearance="fill">
               <mat-label>Kategoria</mat-label>
               <input matInput placeholder="Podaj kategorię produktu" formControlName="category">
-            <div *ngIf="category?.invalid && (category?.dirty || category?.touched)" class="errorMessages">
-              <div *ngIf="category?.errors?.['required']">Kategoria jest wymagana</div>
-              <div *ngIf="category?.errors?.['minlength']">Kategoria musi mieć przynajmniej 4 znaki</div>
-            </div>
+              <div *ngIf="category?.invalid && (category?.dirty || category?.touched)" class="errorMessages">
+                  <div *ngIf="category?.errors?.['required']">Kategoria jest wymagana</div>
+                  <div *ngIf="category?.errors?.['minlength']">Kategoria musi mieć przynajmniej 4 znaki</div>
+              </div>
           </mat-form-field>
 
           <mat-form-field appearance="fill">
               <mat-label>Cena</mat-label>
               <input matInput placeholder="Podaj cenę produktu" formControlName="price">
-            <div *ngIf="price?.invalid && (price?.dirty || price?.touched)" class="errorMessages">
-              <div *ngIf="price?.errors?.['required']">Cena jest wymagana</div>
-              <div *ngIf="price?.errors?.['min']">Cena musi być większa od zera</div>
-            </div>
+              <div *ngIf="price?.invalid && (price?.dirty || price?.touched)" class="errorMessages">
+                  <div *ngIf="price?.errors?.['required']">Cena jest wymagana</div>
+                  <div *ngIf="price?.errors?.['min']">Cena musi być większa od zera</div>
+              </div>
           </mat-form-field>
 
           <mat-form-field appearance="fill">
               <mat-label>Waluta</mat-label>
               <input matInput placeholder="Podaj walutę produktu" formControlName="currency">
-            <div *ngIf="currency?.invalid && (currency?.dirty || currency?.touched)" class="errorMessages">
-              <div *ngIf="currency?.errors?.['required']">Waluta jest wymagana</div>
-            </div>
+              <div *ngIf="currency?.invalid && (currency?.dirty || currency?.touched)" class="errorMessages">
+                  <div *ngIf="currency?.errors?.['required']">Waluta jest wymagana</div>
+              </div>
           </mat-form-field>
 
           <div fxLayout="colum" fxFlexAlign="end" fxLayoutGap="10">
